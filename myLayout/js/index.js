723df667affca4740f2c70932ccd00d0afbbb555
Ilya <68436348+ilya-efimenko@users.noreply.github.com>
2020-07-18 12:51:22 +0300

    Add files via upload
    updates

--- myLayout/js/index.js
+++ myLayout/js/index.js
@@ -2,7 +2,7 @@ const burger = document.getElementById('burger_container')
 const top_nav = document.querySelector('.top_nav')
 const icons = document.querySelector('.icons')
 burger.onclick = function burgerClicked(event) {
-    event.target.classList.toggle('change')
+    this.classList.toggle('change')
     top_nav.classList.toggle('change')
     icons.classList.toggle('change')
 }

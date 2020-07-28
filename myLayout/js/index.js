7e06072d51aa0e801e3b66526e0d32f3b4eef560
ilya-efimenko <ilya.efimenko777@gmail.com>
2020-07-28 13:25:20 +0300

    updates

--- myLayout/js/index.js
+++ myLayout/js/index.js
@@ -8,4 +8,38 @@ burger.onclick = function burgerClicked(event) {
     icons.classList.toggle('change')
 }
 
-// const img_bitbucked
+let openedText
+let buttonOpenedtext
+let clickedP = false
+const question_answer_div = document.querySelectorAll('.question_answer .question_answer_text')
+const question_answer_div_link = document.querySelectorAll('.question_answer a')
+question_answer_div_link.forEach(link => {
+    link.addEventListener('click', function (event) {
+        event.preventDefault()
+    })
+})
+question_answer_div.forEach(prop => {
+    prop.addEventListener('click', function (event) {
+        if(clickedP) {
+            event.target.style.color = '#8E96B7'
+            openedText.style.display = 'none'
+            buttonOpenedtext.style.display = 'none'
+            clickedP = false
+        } else {
+            event.target.style.color = '#495274'
+            openedText = event.target.appendChild(document.createElement('p'))
+            openedText.className = 'open_text'
+            openedText.innerText = 'Quidam vocibus eum ne, erat consectetuer voluptatibus ut nam. Eu usu vidit tractatos, vero tractatos ius an, in mel diceret persecuti.'
+            openedText.classList.toggle('open')   
+            buttonOpenedtext = event.target.appendChild(document.createElement('button'))
+            buttonOpenedtext.className = 'open_text_button'
+            buttonOpenedtext.classList.toggle('open')
+            buttonOpenedtext.innerText = 'Go to documentation'
+            clickedP = true
+        }
+    })
+})
+
+
+
+
